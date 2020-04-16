76c00de7bbb9e8aef09d56460d66b6d43e9f8cda
jungmingu <wjdalsrn35@naver.com>
2020-04-16 12:39:03 +0900

    revising_jungmingu

--- app/src/main/java/com/example/dongbanja/MainActivity.java
+++ app/src/main/java/com/example/dongbanja/MainActivity.java
@@ -1,16 +1,44 @@
 package com.example.dongbanja;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.acitivity_start);
+
+//시작
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        ProfileActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        };
+        Button button1 = (Button) findViewById(R.id.start_button);
+        button1.setOnClickListener(listener);
+
+
+/*
+        Button b = (Button)findViewById(R.id.start_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        ProfileActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+ */
+
     }
-<<<<<<<<< Temporary merge branch 1
-    int i = 2+2;
+
 }
---
+++ app/src/main/java/com/example/dongbanja/ProfileActivity.java
@@ -0,0 +1,11 @@
+package com.example.dongbanja;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);  // layout xml 과 자바파일을 연결
+    } // end onCreate()
+} // end
