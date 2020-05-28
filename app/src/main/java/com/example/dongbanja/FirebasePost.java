9260ddafbb51d8c1d988f9f7d2b39df519f349e4
jungmingu123 <34199095+jungmingu123@users.noreply.github.com>
2020-05-28 21:12:22 +0900

    sdf
    sdfsdf

--- app/src/main/java/com/example/dongbanja/FirebasePost.java
+++ app/src/main/java/com/example/dongbanja/FirebasePost.java
@@ -8,28 +8,42 @@ import java.util.Map;
 //데이터 전송
 @IgnoreExtraProperties
 public class FirebasePost {
- //   public String id;
-    public String name;
- //   public Long age;
- //   public String gender;
+    public String name, gender, education, phonenumber, length, job, money, family, drunksmoke, living, salary, religion;
 
-    public FirebasePost(){
-        // Default constructor required for calls to DataSnapshot.getValue(FirebasePost.class)
-    }
+    
 
-    public FirebasePost( String name) {
-
+    public FirebasePost(String name) {
         this.name = name;
-
+        this.gender = this.gender;
+        this.education = this.education;
+        this.phonenumber = this.phonenumber;
+        this.length = this.length;
+        this.job = this.job;
+        this.money = this.money;
+        this.family = this.family;
+        this.drunksmoke = this.drunksmoke;
+        this.living = this.living;
+        this.salary = this.salary;
+        this.religion = religion;
     }
+
 
     @Exclude
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-   //     result.put("id", id);
         result.put("name", name);
-    //    result.put("age", age);
-   //     result.put("gender", gender);
+        result.put("gender", gender);
+        result.put("education", education);
+        result.put("phonenumber", phonenumber);
+        result.put("length", length);
+        result.put("job", job);
+        result.put("money", money);
+        result.put("family", family);
+        result.put("drunksmoke", drunksmoke);
+        result.put("living", living);
+        result.put("salary", salary);
+        result.put("religion", religion);
+
         return result;
     }
 }
