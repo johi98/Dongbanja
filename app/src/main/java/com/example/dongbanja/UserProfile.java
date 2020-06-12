c58cc4cdb65276c745e970afbd7da3bf9632ea6d
jungmingu123 <34199095+jungmingu123@users.noreply.github.com>
2020-06-13 01:19:39 +0900

    Revert "데이터베이스 쓰기 UID 추가"
    This reverts commit 28847b94f57f1219e994f89d4b88d1ced560ca08.

--- app/src/main/java/com/example/dongbanja/UserProfile.java
+++ app/src/main/java/com/example/dongbanja/UserProfile.java
@@ -1,19 +1,16 @@
 package com.example.dongbanja;
 
 import com.google.firebase.database.Exclude;
-import com.google.firebase.database.IgnoreExtraProperties;
 
 import java.util.HashMap;
 import java.util.Map;
-@IgnoreExtraProperties
+
 public class UserProfile {
     public String name, education, phonenumber, length, job, money, family, drunksmoke, living, salary, religion;
-    public int starCount = 0;
-    public Map<String, Boolean> stars = new HashMap<>();
 
-    public UserProfile() {
-        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
-    }
+
+
+    public UserProfile(){}
 
     public UserProfile(String name, String education, String phonenumber, String length, String job, String money, String family, String drunksmoke, String living, String salary, String religion) {
         this.name = name;
@@ -29,23 +26,35 @@ public class UserProfile {
         this.religion = religion;
     }
 
-    @Exclude
-    public Map<String, Object> toMap() {
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("name", name);
-        result.put("education", education);
-        result.put("phonenumber", phonenumber);
-        result.put("length", length);
-        result.put("job", job);
-        result.put("money", money);
-        result.put("family", family);
-        result.put("drunksmoke", drunksmoke);
-        result.put("living", living);
-        result.put("salary", salary);
-        result.put("religion", religion);
-
-
-
-        return result;
+    public String getName() {return name; }
+    public String getEducation() {
+        return education;
+    }
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+    public String getLength() {
+        return length;
+    }
+    public String getJob() {
+        return job;
+    }
+    public String getMoney() {
+        return money;
+    }
+    public String getFamily() {
+        return family;
+    }
+    public String getDrunksmoke() {
+        return drunksmoke;
+    }
+    public String getLiving() {
+        return living;
+    }
+    public String getSalary() {
+        return salary;
+    }
+    public String getReligion() {
+        return religion;
     }
 }
